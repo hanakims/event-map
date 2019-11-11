46d0449d9ee130cfe66156827b801a5323e5d148
Paridhi Khaitan <paridhi4699@gmail.com>
2019-11-11 01:17:21 -0800

    Login and refactoring

---
+++ app/src/main/java/com/example/eventfinder/PopulateEvents.java
@@ -0,0 +1,14 @@
+package com.example.eventfinder;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PopulateEvents extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_populate_events);
+    }
+}
